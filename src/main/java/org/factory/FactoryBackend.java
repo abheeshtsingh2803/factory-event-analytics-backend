67304e39a5591800f3ae2206943ca6b7a27306d3
Abheesht Singh <abheesht.singh2803@gmail.com>
2026-01-13 17:37:34 +0530

    Initial project setup with Spring Boot and Gradle
    Set up a new Java Spring Boot project with Gradle build system. Added basic project structure, Gradle wrapper, application entry point, and configuration files for PostgreSQL database and application settings.

---
+++ src/main/java/org/factory/FactoryBackend.java
@@ -0,0 +1,11 @@
+package org.factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryBackend {
+    public static void main(String[] args) {
+        SpringApplication.run(FactoryBackend.class, args);
+    }
+}
